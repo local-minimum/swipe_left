87d286e14ac9691e89d5f3f71eb155f8cce7a33b
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-09-24 17:35:39 +0200

    Basic sequencer

---
+++ Assets/scripts/SwipeStage.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class SwipeStage : MonoBehaviour {
+
+    [SerializeField]
+    Text CurrentUserName;
+
+    [SerializeField]
+    Image CurrentPhoto;
+
+    [SerializeField]
+    Sprite[] UserPhotos;
+
+    [SerializeField]
+    string[] UserNames;
+
+    int showingIndex = -1;
+
+    Animator anim;
+
+    [SerializeField, Range(0, 1)]
+    float swapDelay = 0.3f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(DisplayNextInQueue());
+    }
+
+    void OnEnable()
+    {
+        GetComponentInChildren<swiper>().OnSwipeVote += SwipeStage_OnSwipeVote;
+    }
+
+    void OnDisable()
+    {
+        GetComponentInChildren<swiper>().OnSwipeVote -= SwipeStage_OnSwipeVote;
+    }
+
+    private void SwipeStage_OnSwipeVote(bool liked)
+    {
+        //TODO: Do something with the liking
+        StartCoroutine(DisplayNextInQueue());
+    }
+
+    IEnumerator<WaitForSeconds> DisplayNextInQueue()
+    {
+        anim.SetTrigger("Intermission");
+        showingIndex++;
+
+        yield return new WaitForSeconds(swapDelay);
+        //TODO: This should not be in the future
+        showingIndex %= UserNames.Length;
+
+        CurrentUserName.text = UserNames[showingIndex];
+        CurrentPhoto.sprite = UserPhotos[showingIndex];
+    }
+}
