963faa951a27231565c78b27554492a4de51cc2b
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-09-29 00:16:08 +0200

    Animating option selection

---
+++ Assets/scripts/UIButtonish.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonish : MonoBehaviour {
+
+    Animator anim;
+    string EnterTrigger = "Enter";
+    string ExitTrigger = "Exit";
+
+    void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+    }
+
+    public void OnMouseOver()
+    {
+        anim.SetTrigger(EnterTrigger);        
+    }
+
+    public void OnMouseExit()
+    {
+        anim.SetTrigger(ExitTrigger);
+    }
+}
