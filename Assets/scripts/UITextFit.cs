75a1e6c4b3fc3d1c5481d14739b4263b628fdda5
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-09-28 23:13:01 +0200

    Fitting of text content heights

---
+++ Assets/scripts/UITextFit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextFit : MonoBehaviour {
+
+    [SerializeField]
+    float overshoot = 10;
+
+    void Reset()
+    {
+        UpdateSize();
+    }
+
+    void Start() {
+        UpdateSize();
+    }
+
+    void UpdateSize() { 
+        Text text = GetComponentInChildren<Text>();
+        LayoutElement lElem = GetComponent<LayoutElement>();
+
+        float innerHeight = LayoutUtility.GetPreferredHeight(text.rectTransform);
+        float padding = text.rectTransform.offsetMin.y + text.rectTransform.offsetMax.y;
+        float anchorHeight = text.rectTransform.anchorMax.y - text.rectTransform.anchorMin.y;
+        lElem.minHeight = overshoot + padding +  innerHeight / anchorHeight;
+	}
+	
+    public void SetText(string txt)
+    {
+        Text text = GetComponentInChildren<Text>();
+        text.text = txt;
+        UpdateSize();
+    }
+}
