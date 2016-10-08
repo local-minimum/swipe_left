eab292edb4ee73321ab4db81c82a2c8bc091ce72
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-10-08 12:06:36 +0200

    Preparing for switching to profile image swiping

---
+++ Assets/editor/TransAnimEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TransparencyAnim))]
+public class TransAnimEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Test"))
+        {
+            (target as TransparencyAnim).Anim();
+        }
+    }
+}
---
+++ Assets/scripts/TransparencyAnim.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class TransparencyAnim : MonoBehaviour {
+
+    [SerializeField]
+    float duration = 1f;
+
+    [SerializeField]
+    AnimationCurve curve;
+
+    [SerializeField]
+    Image img;
+
+
+    public void Anim()
+    {
+        StartCoroutine(anim());
+    }
+
+    IEnumerator<WaitForSeconds> anim()
+    {
+        float startTime = Time.timeSinceLevelLoad;
+        Debug.Log("Start");
+        while (true)
+        {
+            float progress = Mathf.Clamp01((Time.timeSinceLevelLoad - startTime) / duration);
+
+            Color color = img.color;
+            color.a = curve.Evaluate(progress);
+            img.color = color;
+            if (progress < 1)
+            {
+                yield return new WaitForSeconds(0.02f);
+            } else
+            {
+                break;
+            }            
+        }
+        Debug.Log("End");
+    }
+}
